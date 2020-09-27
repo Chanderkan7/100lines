a5b4bf1a2f6f47bf8dfdef1c8fa0f918ab424ba4
Chanderkant Tiwari <67823569+Chanderkan7@users.noreply.github.com>
2020-09-27 17:30:02 +0530

    Abstraction, Encapsulation, Inheritence
    Abstraction, Encapsulation, Inheritence

---
+++ September-2020/27-Sept-2020/main.cpp
@@ -0,0 +1,110 @@
+#include<iostream>
+using namespace std;
+class Animal
+{
+	public:
+		void animalSound()
+		{
+			cout << "This is a generic function!";
+		}
+};
+class Dog: public Animal
+{
+	public:
+		void animalSound()
+		{
+			cout << "Woof";
+		}
+};
+int main(void)
+{
+	Animal *obj;
+	obj = new Dog();
+	obj -> animalSound();
+	return 0;
+}
+/*#include<iostream>
+using namespace std;
+class Animal
+{
+	public:
+		virtual void animalSound()
+		{
+			cout << "This is a generic function";
+		}	
+};
+class Dog: public Animal
+{
+	public:
+		void animalSound()
+		{
+			cout << "Woof";
+		}
+};
+int main()
+{
+	Animal *obj;
+	obj = new Dog();
+	obj -> animalSound();
+	return 0;
+}
+/*#include<iostream>
+using namespace std;
+class ExampleEncap
+{
+	private:
+		int num;
+		char ch;
+	public:
+		int getNum() const
+		{
+			return num;
+		}
+		char getCh() const
+		{
+			return ch;
+		}
+		void setNum(int num)
+		{
+			this -> num = num;
+		}
+		void setCh(char ch)
+		{
+			this -> ch = ch;
+		}
+};
+int main()
+{
+	ExampleEncap obj;
+	obj.setNum(100);
+	obj.setCh('A');
+	cout << obj.getCh() << endl;
+	cout << obj.getNum() << endl;
+	return 0;
+}
+/*#include<iostream>
+using namespace std;
+class AbstractionExample
+{
+	private:
+		int num;
+		char ch;
+	public:
+		void setMyValues(int n, char c)
+		{
+			num = n;
+			ch = c;
+		}
+		void getMyValues()
+		{
+			cout << "Number is: " << num << endl;
+			cout << "Char is: " << num << endl;
+		}
+};
+int main()
+{
+	AbstractionExample obj;
+	obj.setMyValues(100, 'X');
+	obj.getMyValues();
+	return 0;
+}*/
